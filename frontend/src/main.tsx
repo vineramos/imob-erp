b0f4f64052ad76040f99860b3aadeb7b8fcb5b78
vineramos <97920119+vineramos@users.noreply.github.com>
2026-08-28 23:32:59 -0300

    feat: mount global ERP theme provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { ThemeProvider } from './theme/ThemeProvider'
 import './styles.css'
 import './settings.css'
 import './foundation.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
