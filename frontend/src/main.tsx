e4a049201dd145733c0fff60920005dc68dd658d
vineramos <97920119+vineramos@users.noreply.github.com>
2026-08-28 23:19:20 -0300

    feat: load foundation state styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './settings.css'
+import './foundation.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
