95661d6a23add3d2e1856c8f840dc31802e0c757
vineramos <97920119+vineramos@users.noreply.github.com>
2026-09-08 02:06:56 -0300

    Integrar busca de proprietário e acabamento de Imóveis

--- frontend/src/modules/properties/PropertiesPage.tsx
+++ frontend/src/modules/properties/PropertiesPage.tsx
@@ -1,12 +1,14 @@
 import { PeopleWorkspacePage } from './PeopleWorkspacePage'
 import { PropertyGalleryMount } from './PropertyGalleryMount'
 import { PropertyMapPublicationMount } from './PropertyMapPublicationMount'
+import { PropertyOwnerAutocompleteMount } from './PropertyOwnerAutocompleteMount'
 import { PropertyWorkspacePage } from './PropertyWorkspacePage'
+import './property-homologation.css'
 
 type Props = { permissions: string[]; initialTab?: 'properties' | 'people' }
 
 export function PropertiesPage({ permissions, initialTab = 'properties' }: Props) {
   return initialTab === 'people'
     ? <PeopleWorkspacePage permissions={permissions} />
-    : <><PropertyWorkspacePage permissions={permissions} /><PropertyGalleryMount permissions={permissions} /><PropertyMapPublicationMount permissions={permissions} /></>
+    : <><PropertyWorkspacePage permissions={permissions} /><PropertyOwnerAutocompleteMount /><PropertyGalleryMount permissions={permissions} /><PropertyMapPublicationMount permissions={permissions} /></>
 }
